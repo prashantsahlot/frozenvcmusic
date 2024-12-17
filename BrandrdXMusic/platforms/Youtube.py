a97a7e71451a0cdb24b1a97d2522f938d8fcd4d7
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-12-17 20:48:48 +0530

    Update Youtube.py

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -297,15 +297,15 @@ class YouTubeAPI:
             link = self.base + link
         loop = asyncio.get_running_loop()
         def audio_dl():
-    ydl_optssx = {
-        "format": "bestaudio[abr<=128]/best",  # Set audio bitrate to 128 kbps
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        "quiet": True,
-        "cookiefile": cookie_txt_file(),
-        "no_warnings": True,
-    }
+            ydl_optssx = {
+                "format": "bestaudio/best",
+                "outtmpl": "downloads/%(id)s.%(ext)s",
+                "geo_bypass": True,
+                "nocheckcertificate": True,
+                "quiet": True,
+                "cookiefile" : cookie_txt_file(),
+                "no_warnings": True,
+            }
             x = yt_dlp.YoutubeDL(ydl_optssx)
             info = x.extract_info(link, False)
             xyz = os.path.join("downloads", f"{info['id']}.{info['ext']}")
